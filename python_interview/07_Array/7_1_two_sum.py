a56ea728f43359b482bf653867d59e05914cc673
jdh0120 <dhjung081121@gmail.com>
2022-02-15 20:45:35 +0900

    PAI_220215 7_1

--- python_interview/07_Array/7_1_two_sum.py
+++ python_interview/07_Array/7_1_two_sum.py
@@ -1,17 +1,19 @@
-def sol1(self, arr:list[int], target:int)->list[int]:
-    # Brute-force
+def sol1(arr:list[int], target:int)->list[int]:
+    for i in range(len(arr)):
+        for j in range(i+1,len(arr)-1):
+            if arr[i]+arr[j]==target:
+                return [i,j]
+    
+def sol2(arr:list[int], target:int)->list[int]:
     pass
 
-def sol2(self, arr:list[int], target:int)->list[int]:
+def sol3(arr:list[int], target:int)->list[int]:
     pass
 
-def sol3(self, arr:list[int], target:int)->list[int]:
-    pass
-
-def sol4(self, arr:list[int], target:int)->list[int]:
+def sol4(arr:list[int], target:int)->list[int]:
     pass
 
 
 arr=[2,7,11,15,3,8,5]
 target = 9
-sol1(arr,target)
+print(sol1(arr,target))
